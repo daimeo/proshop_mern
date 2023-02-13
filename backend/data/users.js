4fad18daaf014939f5afb8b7869a2180d0c01b44
Tester SGB <testersaigonbpo@gmail.com>
2023-02-13 17:53:25 +0700

    minor change to match VN names

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,15 +2,21 @@ import bcrypt from "bcryptjs";
 import { faker } from "@faker-js/faker";
 
 faker.locale = "vi";
-let users = [];
-
-const firstName = faker.name.firstName();
-const lastName = faker.name.lastName();
+let users = [
+    {
+        name: "Admin User",
+        email: "admin@example.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: true,
+    },
+];
 
 for (let i = 0; i < 5; i++) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
     users.push({
-        name: firstName + lastName,
-        email: faker.internet.exampleEmail(firstName),
+        name: lastName + " " + firstName,
+        email: faker.internet.exampleEmail(firstName, lastName),
         password: bcrypt.hashSync("123456", 10),
         isAdmin: false,
     });
