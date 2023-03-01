18688a91ef85774f3b6f111f88f78e0b6ead2055
Tester SGB <testersaigonbpo@gmail.com>
2023-03-01 10:23:16 +0700

    TEST: update for cloud build

--- frontend/postinstall.js
+++ frontend/postinstall.js
@@ -2,8 +2,13 @@ const fse = require("fs-extra");
 const path = require("path");
 const topDir = __dirname;
 fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
+// fse.copySync(
+//     path.join(topDir, "node_modules", "tinymce"),
+//     path.join(topDir, "public", "tinymce"),
+//     { overwrite: true }
+// );
 fse.copySync(
     path.join(topDir, "node_modules", "tinymce"),
-    path.join(topDir, "public", "tinymce"),
+    path.join(topDir, "build", "tinymce"),
     { overwrite: true }
 );
