e4070d10907cbadf5022ad6d6dcb0132ba44bb29
Tester <testersaigonbpo@gmail.com>
2023-03-06 14:45:49 +0700

    Cloud Build Experiment: changes to see if it works

--- backend/server.js
+++ backend/server.js
@@ -63,17 +63,17 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 console.log("DIRNAME: " + __dirname);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-    );
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running....");
-    });
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/frontend/build")));
+//
+//     app.get("*", (req, res) =>
+//         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//     );
+// } else {
+//     app.get("/", (req, res) => {
+//         res.send("API is running....");
+//     });
+// }
 
 app.use(notFound);
 app.use(errorHandler);
