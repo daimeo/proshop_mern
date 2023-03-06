17525ce73ae8dca36680b9a23ca77ceb7ff22e74
Tester <testersaigonbpo@gmail.com>
2023-03-06 18:00:09 +0700

    Cloud Build Experiment: no use express.static and use app.yaml handlers

--- backend/server.js
+++ backend/server.js
@@ -59,9 +59,11 @@ app.get("/api/config", (req, res) =>
     })
 );
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-console.log("DIRNAME: " + __dirname);
+if (process.env.NODE_ENV !== "production") {
+    const __dirname = path.resolve();
+    app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+    console.log("DIRNAME: " + __dirname);
+}
 
 if (process.env.NODE_ENV === "testing") {
     app.use(express.static(path.join(__dirname, "/frontend/build")));
